333e6c8c61b2c2e04ac9d3fc443bd7c6c0b38777
Mahdi Rezaei <mahdi.sohaily4030@gmail.com>
2023-07-20 14:50:32 +0330

    Add script to show the number of factors for specific date

--- tvAjax.php
+++ tvAjax.php
@@ -8,6 +8,26 @@ if (isset($_POST['user'])) {
     $user  = getinternal($_SESSION["id"]);
 }
 
+function givenPrice($con)
+{
+    $sql = "SELECT 
+ prices.price, prices.partnumber, users.username,customer.id AS customerID, users.id as userID, prices.created_at, customer.name, customer.family
+ FROM ((shop.prices 
+ INNER JOIN callcenter.customer ON customer.id = prices.customer_id )
+ INNER JOIN yadakshop1402.users ON users.id = prices.user_id)
+ ORDER BY prices.created_at DESC LIMIT 100";
+    $result = mysqli_query($con, $sql);
+
+
+    $givenPrices = [];
+    if (mysqli_num_rows($result) > 0) {
+        while ($item = mysqli_fetch_assoc($result)) {
+            array_push($givenPrices, $item);
+        }
+    }
+    return  $givenPrices;
+}
+
 $sql = "SELECT * FROM shop.tv WHERE id='1'";
 $factor_result = mysqli_query($con, $sql);
 $tv = mysqli_fetch_assoc($factor_result);
@@ -231,25 +251,7 @@ if ($status == 'on') :
                     <tbody>
                         <?php
                         $givenPrice = givenPrice($con);
-                        function givenPrice($con)
-                        {
-                            $sql = "SELECT 
-                         prices.price, prices.partnumber, users.username,customer.id AS customerID, users.id as userID, prices.created_at, customer.name, customer.family
-                         FROM ((shop.prices 
-                         INNER JOIN callcenter.customer ON customer.id = prices.customer_id )
-                         INNER JOIN yadakshop1402.users ON users.id = prices.user_id)
-                         ORDER BY prices.created_at DESC LIMIT 100";
-                            $result = mysqli_query($con, $sql);
 
-
-                            $givenPrices = [];
-                            if (mysqli_num_rows($result) > 0) {
-                                while ($item = mysqli_fetch_assoc($result)) {
-                                    array_push($givenPrices, $item);
-                                }
-                            }
-                            return  $givenPrices;
-                        }
 
                         if (count($givenPrice) > 0) {
                         ?>
