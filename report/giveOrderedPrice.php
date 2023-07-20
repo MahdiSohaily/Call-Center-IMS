ac071190a9a3b4d9bebf0d50288a81afd39cf89b
Mahdi Rezaei <mahdi.sohaily4030@gmail.com>
2023-07-20 10:34:27 +0330

    Update the given order price

--- report/giveOrderedPrice.php
+++ report/giveOrderedPrice.php
@@ -117,7 +117,7 @@ if ($isValidCustomer) {
                             $customer = $customer;
                             $completeCode = $completeCode;
                     ?>
-                            <div class="grid grid-cols-1 grid-cols-1 lg:grid-cols-9 gap-6 lg:gap-2 lg:p-2 overflow-auto px-5">
+                            <div class="grid grid-cols-1 grid-cols-1 lg:grid-cols-9 gap-6 lg:gap-2 lg:p-2 overflow-auto">
 
                                 <!-- Start the code info section -->
                                 <div class="min-w-full bg-white rounded-lg col-span-2 overflow-auto shadow-md mt-2">
