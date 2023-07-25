0dd9fab38ad0cbb07a2d4d5faa62fe180492a6dc
Mahdi Rezaei <mahdi.sohaily4030@gmail.com>
2023-07-25 10:14:49 +0330

    Add new leet code solutions

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var merge = function (intervals) {
+  // sorting the intervals elements members
   intervals = intervals.map((interval) => {
     interval.sort(function (a, b) {
       return a - b;
@@ -6,47 +7,44 @@ var merge = function (intervals) {
     return interval;
   });
 
-  let size = intervals.length;
+  // sorting the intervals elements.
+  intervals = intervals.sort(function (a, b) {
+    return (
+     a[0] - b[0]
+    );
+  });
 
+  let size = intervals.length; // the size of the interval array
+
+  
+  let collapsed = [];
   let output = [];
 
-  let test = intervals[0];
-
   if (size != 1) {
-    for (let count = 1; count < size; count++) {
-      let index1 = null;
-      let index2 = null;
-
-      if (intervals[count][0] <= test[1]) {
-        const items = [...test, ...intervals[count]];
-        items.sort(function (a, b) {
-          return a - b;
-        });
-
-        index1 = items[0];
-        index2 = items[items.length - 1];
-        test = [index1, index2];
-        output.push(test);
+    collapsed = [intervals[0]];
+    for (let count = 0; count < size; count++) {
+      if (collapsed[collapsed.length - 1][1] >= intervals[count][0]) {
+        collapsed[collapsed.length - 1][1] = Math.max(
+          collapsed[collapsed.length - 1][1],
+          intervals[count][1]
+        );
       } else {
-        if (test !== output[output.length - 1]) {
-          output.push(test);
-        }
-        index1 = intervals[count][0];
-        index2 = intervals[count][1];
-        test = [index1, index2];
-        output.push(test);
+        collapsed.push(intervals[count]);
       }
     }
   } else {
     output.push(...intervals);
   }
 
-  return output;
+  output = [...output, ...collapsed];
+
+  return output.sort(function (a, b) {
+    return (
+      [...a].reduce((a, b) => a + b, 0) - [...b].reduce((a, b) => a + b, 0)
+    );
+  });
 };
 
 console.log(
-  merge([
-    [1, 4],
-    [4, 5],
-  ])
+  merge([[2,3],[2,2],[3,3],[1,3],[5,7],[2,2],[4,6]])
 );
