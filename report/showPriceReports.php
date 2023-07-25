a2d338c9c72e8b87515b16e243e685d47e7af688
Mahdi Rezaei <mahdi.sohaily4030@gmail.com>
2023-07-25 11:26:37 +0330

    Update the grid layout for given price report page

--- report/showPriceReports.php
+++ report/showPriceReports.php
@@ -162,7 +162,7 @@ if ($isValidCustomer) {
                             <div class="grid grid-cols-1 grid-cols-1 lg:grid-cols-10 gap-6 lg:gap-2 lg:p-2 overflow-auto">
 
                                 <!-- Start the code info section -->
-                                <div class="min-w-full bg-white rounded-lg col-span-2 overflow-auto shadow-md mt-2">
+                                <div class="min-w-full bg-white rounded-lg col-span-1 overflow-auto shadow-md mt-2">
                                     <div class="rtl p-3">
                                         <p style="font-size: 0.8rem;" class="text-left bg-gray-600 text-white p-2 my-3 rounded-md">
                                             <?php echo $index; ?>
