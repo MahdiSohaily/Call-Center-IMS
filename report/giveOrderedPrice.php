08e477dd83239b059934bc832ffcf16b7956abc4
Mahdi Rezaei <mahdi.sohaily4030@gmail.com>
2023-08-20 09:54:38 +0330

    Update the style for accordion items

--- report/giveOrderedPrice.php
+++ report/giveOrderedPrice.php
@@ -1,8 +1,8 @@
 ﻿<?php
-require_once './database/connect.php';
 require_once('./views/Layouts/header.php');
-require_once('./app/Controllers/GivenPriceController.php');
 require_once './utilities/helper.php';
+require_once './database/connect.php';
+require_once('./app/Controllers/GivenPriceController.php');
 
 if ($isValidCustomer) {
     if ($finalResult) {
