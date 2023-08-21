4a95259148738cdc24d3f1bf91fababaf68e26eb
Mahdi Rezaei <mahdi.sohaily4030@gmail.com>
2023-08-21 12:05:19 +0330

    Add script to send data to the server side

--- report/showPriceReportsTelegram.php
+++ report/showPriceReportsTelegram.php
@@ -1,32 +1,58 @@
-<!DOCTYPE html>
-<html lang="en">
+<?php
+require_once './database/connect.php';
+require_once('./views/Layouts/header.php');
+require_once('./app/Controllers/GivenPriceController.php');
+require_once './utilities/helper.php';
+?>
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Telegram</title>
-</head>
+<script>
+    // Create a new XMLHttpRequest object
+    var xhr = new XMLHttpRequest();
 
-<body>
-    <script>
-        // Create a new XMLHttpRequest object
-        var xhr = new XMLHttpRequest();
+    // Configure the request
+    xhr.open("GET", "http://telegram.om-dienstleistungen.de/", true); // Change the URL to the external site's API endpoint
 
-        // Configure the request
-        xhr.open("GET", "http://telegram.om-dienstleistungen.de/", true); // Change the URL to the external site's API endpoint
+    // Set up a callback function to handle the response
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            var response = xhr.responseText;
+            console.log(response.length);
+            if (response.length > 2) {
+                console.log('we are sending');
+                // Create a hidden input field to store the JSON data
+                const jsonInput = document.createElement('input');
+                jsonInput.type = 'hidden';
+                jsonInput.name = 'jsonData';
+                jsonInput.value = JSON.stringify(response);
 
-        // Set up a callback function to handle the response
-        xhr.onreadystatechange = function() {
-            if (xhr.readyState === 4 && xhr.status === 200) {
-                var response = xhr.responseText;
-                console.log("Response:", response);
-                // You can do something with the response data here
+                // Append the input field to the form
+                const form = document.createElement('form');
+                form.method = 'post';
+                form.action = ''; // Leave empty to post to the same page
+                form.appendChild(jsonInput);
+
+                // Append the form to the body and submit it
+                document.body.appendChild(form);
+                form.submit();
             }
-        };
+        }
+    };
 
-        // Send the request
-        xhr.send();
-    </script>
-</body>
+    // Send the request
+    xhr.send();
+</script>
+<?php
+if (isset($_POST['jsonData'])) {
+    $jsonData = $_POST['jsonData'];
 
-</html>
+    // Decode the JSON data
+    $decodedData = json_decode($jsonData, true); // Decodes as an associative array
+
+    // Process the decoded JSON data
+    // ...
+
+    // Return a response (optional)
+    $response = "Data received and processed!";
+    echo $response;
+}
+require_once('./views/Layouts/footer.php');
