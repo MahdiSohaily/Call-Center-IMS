926e17c2d19ee628db068c4bda7b19bdef107df6
Mahdi Rezaei <mahdi.sohaily4030@gmail.com>
2023-09-04 16:13:06 +0330

    Add script to sort the given price base on the goods amount in group

--- report/showPriceReports.php
+++ report/showPriceReports.php
@@ -15,7 +15,7 @@ if ($isValidCustomer) {
         $rates = $finalResult['rates'];
 ?>
         <div class="grid grid-cols-6">
-            <div class="m-2 p-3 col-span-2 bg-gray-500 relative">
+            <div class="m-2 p-3 col-span-2 bg-gray-600 relative">
                 <table class="min-w-full text-sm font-light p-2">
                     <thead class="font-medium">
                         <tr>
@@ -37,16 +37,15 @@ if ($isValidCustomer) {
                             } ?>
 
                             <tr class="odd:bg-gray-400">
-                                <td class="px-3 py-2 text-left <?= in_array($code, $not_exist) ? 'text-rose-500' : 'text-white' ?> "><a href="<?= $_SERVER['PHP_SELF'] . '#' . $code ?>"><?php echo $code ?></a></td>
+                                <td class="px-3 py-2 text-left font-bold <?= in_array($code, $not_exist) ? 'text-orange-600' : 'text-white' ?> "><a href="<?= $_SERVER['PHP_SELF'] . '#' . $code ?>"><?php echo $code ?></a></td>
                                 <td class="px-3 py-2 text-left text-white" id="<?php echo $code . '-append' ?>">
                                     <?php
                                     if (in_array($code, $not_exist)) {
-                                        echo "<p class ='text-rose-500'>کد اشتباه</p>";
+                                        echo "<p class ='font-bold text-orange-600 text-'>کد اشتباه</p>";
                                         echo " </td>";
                                     ?>
-                                <td class="text-right  py-2" onclick="closeTab()">
-                                </td>
-                            <?php
+
+                                    <?php
                                     } else {
 
                                         if ($max && current($existing[$code])['givenPrice']) {
@@ -54,15 +53,14 @@ if ($isValidCustomer) {
                                         } else if ($max == 0) {
                                             echo '-' . " </td>";
                                         }
-                            ?>
-                                <td class="text-right  py-2" onclick="closeTab()">
+                                    ?>
+                                </td>
+                                <td class="text-right font-bold py-2" onclick="closeTab()">
                                     <i title="کاپی کردن مقادیر" onclick="copyItemPrice(this)" class="px-4 text-white text-sm material-icons hover:cursor-pointer">content_copy</i>
                                 </td>
                             <?php
                                     }
                             ?>
-
-
                             </tr>
                         <?php
                         }
