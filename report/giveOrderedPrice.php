bccfffd30bf6580074344f69f1a1c0a5ab0f2701
Mahdi Rezaei <mahdi.sohaily4030@gmail.com>
2023-09-04 14:26:58 +0330

    Add script to sort the given price base on the goods amount in group

--- report/giveOrderedPrice.php
+++ report/giveOrderedPrice.php
@@ -37,23 +37,32 @@ if ($isValidCustomer) {
                             } ?>
 
                             <tr class="odd:bg-gray-400">
-                                <td class="px-3 py-2 text-left text-white"><a href="<?= $_SERVER['PHP_SELF'] . '#' . $code ?>"><?php echo $code ?></a></td>
+                                <td class="px-3 py-2 text-left <?= in_array($code, $not_exist) ? 'text-rose-300' : 'text-white' ?> "><a href="<?= $_SERVER['PHP_SELF'] . '#' . $code ?>"><?php echo $code ?></a></td>
                                 <td class="px-3 py-2 text-left text-white" id="<?php echo $code . '-append' ?>">
                                     <?php
                                     if (in_array($code, $not_exist)) {
-                                        echo '';
+                                        echo "<p class ='text-rose-300'>کد اشتباه</p>";
+                                        echo " </td>";
+                                    ?>
+                                <td class="text-right  py-2" onclick="closeTab()">
+                                </td>
+                            <?php
                                     } else {
 
                                         if ($max && current($existing[$code])['givenPrice']) {
-                                            echo trim(current(current($existing[$code])['givenPrice'])['price']) !== 'موجود نیست' ? current(current($existing[$code])['givenPrice'])['price'] : '-';
+                                            echo trim(current(current($existing[$code])['givenPrice'])['price']) !== 'موجود نیست' ? current(current($existing[$code])['givenPrice'])['price'] : '';
                                         } else if ($max == 0) {
-                                            echo '-';
+                                            echo '-' . " </td>";
                                         }
-                                    }
-                                    ?>
+                            ?>
                                 <td class="text-right  py-2" onclick="closeTab()">
                                     <i title="کاپی کردن مقادیر" onclick="copyItemPrice(this)" class="px-4 text-white text-sm material-icons hover:cursor-pointer">content_copy</i>
                                 </td>
+                            <?php
+                                    }
+                            ?>
+
+
                             </tr>
                         <?php
                         }
