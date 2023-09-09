e816a463efed0bac67f8fc7896cf15667f5c6f23
Mahdi Rezaei <mahdi.sohaily4030@gmail.com>
2023-09-09 14:01:20 +0330

    Add script to change the old way to display data

--- report/giveOrderedPrice.php
+++ report/giveOrderedPrice.php
@@ -107,6 +107,9 @@ if ($isValidCustomer) {
                                             <tbody>
                                                 <?php
                                                 foreach ($goods as $index => $element) {
+                                                    $allOver = $element['details']['allOver'];
+                                                    $ExistDetails = $element['details']['ExistDetails'];
+                                                    $finalAmount = $element['details']['finalAmount'];
                                                 ?>
                                                     <tr>
                                                         <td class="relative px-1 hover:cursor-pointer" data-part="<?php echo $goods[$index]['information']['partnumber'] ?>" onmouseleave="hideToolTip(this)" onmouseover="showToolTip(this)">
@@ -123,6 +126,66 @@ if ($isValidCustomer) {
                                                             </div>
                                                         </td>
 
+                                                        <td class="px-1 pt-2">
+                                                            <table class="min-w-full text-sm font-light p-2">
+                                                                <thead class="font-medium">
+                                                                    <tr>
+                                                                        <?php
+                                                                        if ($allOver > 0) {
+                                                                            foreach ($finalAmount as $brand => $amount) {
+                                                                                if ($amount > 0) { ?>
+                                                                                    <th onclick="appendBrand(this)" data-code="<?php echo $code ?>" data-price="<?php echo $brand ?>" data-part="<?php echo $partNumber ?>" scope="col" class="<?php echo $brand == 'GEN' || $brand == 'MOB' ? $brand : 'brand-default' ?> text-white text-center py-2 relative hover:cursor-pointer" data-key="<?php echo $index ?>" data-part="<?= $partNumber ?>" data-brand="<?php echo $brand ?>" onmouseover="seekExist(this)" onmouseleave="closeSeekExist(this)">
+                                                                                        <?php echo $brand ?>
+                                                                                        <div class="custome-tooltip" id="<?php echo $index . '-' . $brand ?>">
+                                                                                            <table class="rtl min-w-full text-sm font-light p-2">
+                                                                                                <thead class="font-medium bg-violet-800">
+                                                                                                    <tr>
+                                                                                                        <th class="text-right px-3 py-2 tiny-text">فروشنده</th>
+                                                                                                        <th class="text-right px-3 py-2 tiny-text"> موجودی</th>
+                                                                                                        <th class="text-right px-3 py-2 tiny-text">تاریخ</th>
+                                                                                                        <th class="text-right px-3 py-2 tiny-text">زمان سپری شده</th>
+                                                                                                    </tr>
+                                                                                                </thead>
+                                                                                                <tbody>
+                                                                                                    <?php
+                                                                                                    foreach ($ExistDetails as $item) {
+                                                                                                    ?>
+                                                                                                        <?php if ($item !== 0 && $item['brand_name'] === $brand) { ?>
+                                                                                                            <tr class="odd:bg-gray-500 bg-gray-600">
+                                                                                                                <td class="px-3 py-2 tiny-text text-right"><?php echo $item['seller_name'] ?></td>
+                                                                                                                <td class="px-3 py-2 tiny-text text-right"><?php echo $item['qty'] ?></td>
+                                                                                                                <td class="px-3 py-2 tiny-text text-right"><?php echo $item['invoice_date'] ?></td>
+                                                                                                                <td class="px-3 py-2 tiny-text text-right"><?php echo displayTimePassed($item['invoice_date']) ?></td>
+                                                                                                            </tr>
+                                                                                                        <?php } ?>
+                                                                                                    <?php
+                                                                                                    }
+                                                                                                    ?>
+                                                                                                </tbody>
+                                                                                            </table>
+                                                                                        </div>
+                                                                                    </th>
+                                                                        <?php }
+                                                                            }
+                                                                        } else {
+                                                                            echo '<p class="text-red-400 text-center bold"> در حال حاضر موجود نیست </p>';
+                                                                        } ?>
+                                                                    </tr>
+                                                                </thead>
+                                                                <tbody>
+                                                                    <tr class="py-3">
+                                                                        <?php foreach ($finalAmount as $brand => $amount) {
+                                                                            if ($amount > 0) { ?>
+                                                                                <td class="<?php echo $brand == 'GEN' || $brand == 'MOB' ? $brand : 'brand-default' ?> whitespace-nowrap text-white px-3 py-2 text-center">
+                                                                                    <?php echo $amount;
+                                                                                    ?>
+                                                                                </td>
+                                                                        <?php }
+                                                                        } ?>
+                                                                    </tr>
+                                                                </tbody>
+                                                            </table>
+                                                        </td>
 
                                                         <td class="px-1 pt-2">
                                                             <table class="min-w-full text-left text-sm font-light">
