deab01585e227e9d719c15401e23146bf1bbdb2d
MahdiSohaily <mahdi.sohaily4030@gmail.com>
2023-09-28 12:02:41 +0330

    Add the script to create and update good limit alert from the given price page.

--- report/test.php
+++ report/test.php
@@ -1,51 +1,125 @@
 <?php
+session_start();
+require_once './database/connect.php';
 if (isset($_POST['operation'])) {
     $operation = $_POST['operation'];
 
     // Create a new Limit Alert object
     if ($operation == 'create') {
         $type = $_POST['type'];
+        $id = $_POST['id'];
+
+        $original = $_POST['original'];
+        $fake = $_POST['fake'];
+
+        $original_all = $_POST['original_all'];
+        $fake_all = $_POST['fake_all'];
+
+
 
         switch ($type) {
             case 's':
-                
+                createStockLimitSingle($id, $original, $fake);
+                createOverallLimitSingle($id, $original_all, $fake_all);
                 break;
             case 'r':
+                createStockLimitRelation($id, $original, $fake, $original_all, $fake_all);
+                createOverallLimitRelation($id, $original, $fake, $original_all, $fake_all);
                 break;
         }
-        // INSERT INVENTORY ALERT FOR SPECIFIC INVENTORY
-        $stock_id = 9;
-        $limit_sql = $conn->prepare("INSERT INTO good_limit_inventory (pattern_id, original, fake, user_id, stock_id) VALUES (?, ?, ?, ?, ?)");
-        $limit_sql->bind_param('iiiii', $last_id, $original, $fake, $_SESSION['user_id'], $stock_id);
-        $limit_sql->execute();
-
-
-        // INSERT GOODS ALERT WITHIN ALL THE AVAILABLE STOCKS (GENERAL GOODS AMOUNT ALERT)
-        $limit_sql = $conn->prepare("INSERT INTO good_limit_all (pattern_id, original, fake, user_id) VALUES (?, ?, ?, ?)");
-        $limit_sql->bind_param('iiii', $last_id, $original_all, $fake_all, $_SESSION['user_id']);
-        $limit_sql->execute();
     }
 
 
     // Updating the existing Limit Alerts
     if ($operation == 'update') {
         $type = $_POST['type'];
+        $id = $_POST['id'];
+
+        $original = $_POST['original'];
+        $fake = $_POST['fake'];
+
+        $original_all = $_POST['original_all'];
+        $fake_all = $_POST['fake_all'];
 
         switch ($type) {
             case 's':
+                updateStockLimitSingle($id, $original, $fake, $original_all, $fake_all);
+                updateOverallLimitSingle($id, $original, $fake, $original_all, $fake_all);
                 break;
             case 'r':
+                updateStockLimitRelation($id, $original, $fake, $original_all, $fake_all);
+                updateOverallLimitRelation($id, $original, $fake, $original_all, $fake_all);
                 break;
         }
-        // Update the Inventories limit for goods alert for specific pattern
-        $updateInventoryLimit = $conn->prepare("UPDATE good_limit_inventory SET original= ?, fake = ? WHERE pattern_id = ?");
-        $updateInventoryLimit->bind_param('iii', $original, $fake, $pattern_id);
-        $updateInventoryLimit->execute();
-
-
-        // Update the over all alert for goods in specific relation
-        $updateAllLimit = $conn->prepare("UPDATE good_limit_all SET original= ?, fake = ? WHERE pattern_id = ?");
-        $updateAllLimit->bind_param('iii', $original_all, $fake_all, $pattern_id);
-        $updateAllLimit->execute();
     }
+}
+
+
+function createStockLimitSingle($id, $original, $fake)
+{
+    // INSERT INVENTORY ALERT FOR SPECIFIC INVENTORY
+    $stock_id = 9;
+    $limit_sql = CONN->prepare("INSERT INTO good_limit_inventory (nisha_id, original, fake, user_id, stock_id) VALUES (?, ?, ?, ?, ?)");
+    $limit_sql->bind_param('iiiii', $id, $original, $fake, $_SESSION['user_id'], $stock_id);
+    $limit_sql->execute();
+
+
+
+    function createOverallLimitSingle($id, $original_all, $fake_all)
+    {
+        // INSERT GOODS ALERT WITHIN ALL THE AVAILABLE STOCKS (GENERAL GOODS AMOUNT ALERT)
+        $limit_sql = CONN->prepare("INSERT INTO good_limit_all (nisha_id, original, fake, user_id) VALUES (?, ?, ?, ?)");
+        $limit_sql->bind_param('iiii', $id, $original_all, $fake_all, $_SESSION['user_id']);
+        $limit_sql->execute();
+    }
+}
+
+function createStockLimitRelation($id, $original, $fake)
+{
+}
+function createOverallLimitRelation($id, $original_all, $fake_all)
+{
+}
+
+
+function updateStockLimitSingle($id, $original, $fake)
+{
+    // Update the Inventories limit for goods alert for specific pattern
+    $updateInventoryLimit = CONN->prepare("UPDATE good_limit_inventory SET original= ?, fake = ? WHERE nisha_id = ?");
+    $updateInventoryLimit->bind_param('iii', $original, $fake, $id);
+    $updateInventoryLimit->execute();
+}
+function updateOverallLimitSingle($id, $original_all, $fake_all)
+{
+    // Update the over all alert for goods in specific relation
+    $updateAllLimit = CONN->prepare("UPDATE good_limit_all SET original= ?, fake = ? WHERE nisha_id = ?");
+    $updateAllLimit->bind_param('iii', $original_all, $fake_all, $id);
+    $updateAllLimit->execute();
+}
+
+function updateStockLimitRelation($id, $original, $fake)
+{
+    // Update the Inventories limit for goods alert for specific pattern
+    $updateInventoryLimit = CONN->prepare("UPDATE good_limit_inventory SET original= ?, fake = ? WHERE pattern_id = ?");
+    $updateInventoryLimit->bind_param('iii', $original, $fake, $pattern_id);
+    $updateInventoryLimit->execute();
+
+
+    // Update the over all alert for goods in specific relation
+    $updateAllLimit = CONN->prepare("UPDATE good_limit_all SET original= ?, fake = ? WHERE pattern_id = ?");
+    $updateAllLimit->bind_param('iii', $original_all, $fake_all, $pattern_id);
+    $updateAllLimit->execute();
+}
+function updateOverallLimitRelation($id, $original_all, $fake_all)
+{
+    // Update the Inventories limit for goods alert for specific pattern
+    $updateInventoryLimit = CONN->prepare("UPDATE good_limit_inventory SET original= ?, fake = ? WHERE pattern_id = ?");
+    $updateInventoryLimit->bind_param('iii', $original, $fake, $pattern_id);
+    $updateInventoryLimit->execute();
+
+
+    // Update the over all alert for goods in specific relation
+    $updateAllLimit = CONN->prepare("UPDATE good_limit_all SET original= ?, fake = ? WHERE pattern_id = ?");
+    $updateAllLimit->bind_param('iii', $original_all, $fake_all, $pattern_id);
+    $updateAllLimit->execute();
 }
