195c8214517e1c172268e7c248f265e3f4deaa4c
MahdiSohaily <mahdi.sohaily4030@gmail.com>
2023-10-10 13:27:55 +0330

    Add script to toggle between password and text input types

--- report/createUserProfile.php
+++ report/createUserProfile.php
@@ -12,51 +12,77 @@ require_once('./views/Layouts/header.php');
         <form action="" method="post">
 
             <div class="grid grid-cols-6 gap-6">
-                <input type="text" name="form" value="create" hidden>
                 <div class="col-span-6 sm:col-span-4">
                     <label class="block font-medium text-sm text-gray-700">
                         نام
                     </label>
-                    <input name="partNumber" class="mt-1 block w-full border-gray-300 rounded-md shadow-sm px-3 py-2" required id="serial" type="text" />
+                    <input name="partNumber" class="border-1 mt-1 block w-full border-gray-300 rounded-md shadow-sm px-3 py-2" id="serial" type="text" />
                     <p class="mt-2"></p>
                 </div>
-
                 <!-- Price -->
                 <div class="col-span-6 sm:col-span-4">
                     <label class="block font-medium text-sm text-gray-700">
                         نام خانوادگی
                     </label>
-                    <input name="price" class="mt-1 block w-full border-gray-300 rounded-md shadow-sm px-3 py-2" id="price" type="text" />
+                    <input name="price" class="border-1 mt-1 block w-full border-gray-300 rounded-md shadow-sm px-3 py-2" id="price" type="text" />
                     <p class="mt-2"> </p>
                 </div>
                 <!-- Weight -->
                 <div class="col-span-6 sm:col-span-4">
                     <label class="block font-medium text-sm text-gray-700">
-                        وزن جنس
+                        نام کاربری
                     </label>
-                    <input name="weight" class="mt-1 block w-full border-gray-300 rounded-md shadow-sm px-3 py-2" id="weight" type="text" />
+                    <input required name="weight" class="border-1 mt-1 block w-full border-gray-300 rounded-md shadow-sm px-3 py-2" id="weight" type="text" />
                     <p class="mt-2"> </p>
                 </div>
                 <!-- Mobis -->
-                <div class="col-span-6 sm:col-span-4">
+                <div class="col-span-6 sm:col-span-4 relative">
                     <label class="block font-medium text-sm text-gray-700">
-                        موبیز
+                        رمزعبور
                     </label>
-                    <input name="mobis" class="mt-1 block w-full border-gray-300 rounded-md shadow-sm px-3 py-2" id="mobis" type="text" />
-                    <p class="mt-2"> </p>
+                    <i onclick="togglePass(this)" class="material-icons cursor-pointer" style="position: absolute; left:5px; top: 50%">remove_red_eye</i>
+                    <input required name="mobis" class="border-1 mt-1 block w-full border-gray-300 rounded-md shadow-sm px-3 py-2" id="mobis" type="password" />
                 </div>
                 <!-- Korea section -->
                 <div class="col-span-6 sm:col-span-4">
                     <label class="block font-medium text-sm text-gray-700">
-                        کوریا
+                        نوعیت حساب کاربری
                     </label>
-                    <input name="korea" class="mt-1 block w-full border-gray-300 rounded-md shadow-sm px-3 py-2" id="korea" v-model="form.korea" type="text" />
+                    <select type="status" class="border-1 p-2 text-sm border-gray-300 mt-1 block w-full border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm" id="status">
+                        <option value="1" class="text-sm">
+                            تماس با بازار </option>
+
+                        <option value="2" class="text-sm">
+                            حسابداری </option>
+                        <option value="2" class="text-sm">
+                            انبار </option>
+
+                        <option value="5" class="text-sm">
+                            مدیر </option>
+
+                    </select>
                     <p class="mt-2"> </p>
+                </div>
+                <div class="col-span-6 sm:col-span-4">
+                    <button class="button bg-green-700 text-white py-2 px-3 rounded-lg hover:bg-green-600" type="submit">ایجاد حساب کاربری</button>
                 </div>
             </div>
         </form>
     </div>
 </div>
+<script>
+    function togglePass(element) {
+        const target = element.nextElementSibling;
+        const inputType = target.type;
+
+        if (inputType === 'password') {
+            target.type = 'test';
+            return;
+        }
+
+        target.type = 'password';
+    }
+</script>
 
 <?php
 require_once('./views/Layouts/footer.php');
