445b2fa52b805e58c05ad9907e75e98f7494b2f2
MahdiSohaily <mahdi.sohaily4030@gmail.com>
2023-10-24 13:55:43 +0330

    Add the hard refresh functionality

--- report/hamkarTelegram.php
+++ report/hamkarTelegram.php
@@ -7,7 +7,7 @@ require_once('./app/Controllers/GoodController.php');
 <div class="max-w-7xl my-5 mx-auto bg-white rounded-lg shadow-lg ">
     <div class="flex rtl bg-violet-600  rounded-t-lg p-2">
         <button class="px-4 py-2 rounded-lg bg-green-500 text-white hover:bg-green-300 ml-2 focus:outline-none" onclick="openTab('tab1')">لیست مخاطبین</button>
-        <button class="px-4 py-2 rounded-lg bg-green-500 text-white hover:bg-green-300 ml-2 focus:outline-none" onclick="openTab('tab2')">بروزرسانی لیست مخاطبین</button>
+        <button class="px-4 py-2 rounded-lg bg-green-500 text-white hover:bg-green-300 ml-2 focus:outline-none" onclick="openTab('tab2'); getContacts()">بروزرسانی لیست مخاطبین</button>
         <button class="px-4 py-2 rounded-lg bg-green-500 text-white hover:bg-green-300 ml-2 focus:outline-none" onclick="openTab('tab3')">ارسال پیام</button>
     </div>
     <div class="p-4 rtl">
@@ -18,7 +18,13 @@ require_once('./app/Controllers/GoodController.php');
             </div>
         </div>
         <div id="tab2" class="tab-content hidden">
-            <h1 class="text-xl py-2">مخاطبین اخیر تلگرام</h1>
+            <div class="flex justify-between">
+                <h1 class="text-xl py-2">مخاطبین اخیر تلگرام</h1>
+                <span class="flex items-center cursor-pointer text-white bg-violet-600 rounded-md px-3" onclick="hardRefresh()">
+                    بروزرسانی
+                    <i class="material-icons ">sync</i>
+                </span>
+            </div>
             <div class="my-3">
                 <table class="table-fixed rtl min-w-full text-sm font-light">
                     <thead class="font-medium sticky dark:border-neutral-500 bg-violet-200">
@@ -61,6 +67,60 @@ require_once('./app/Controllers/GoodController.php');
 </div>
 
 <script>
+    const contact = document.getElementById('results_new');
+    let isLoadedTelegramContacts = false;
+
+
+    function hardRefresh() {
+        isLoadedTelegramContacts = false;
+        getContacts();
+    }
+
+    function getContacts() {
+        if (!isLoadedTelegramContacts) {
+            contact.innerHTML = `
+            <tr>
+                <td colspan="7" class="py-5">
+                    <img class=' block w-10 mx-auto h-auto' src="./public/img/loading.png" />
+                </td>
+            </tr>
+            `;
+            axios.post("http://localhost/telegram/")
+                .then(function(response) {
+
+                    displayTelegramData(response.data);
+                    isLoadedTelegramContacts = true;
+                })
+                .catch(function(error) {
+
+                });
+        }
+    }
+
+    function displayTelegramData(data) {
+        let template = ``;
+        let counter = 1;
+        for (let user of data) {
+            template += `
+            <tr class="even:bg-indigo-100" 
+                data-chat=" ${user.chat_id}"
+                data-name=" ${user.title}"
+                data-username=" ${user.username}"
+                data-profile=" ${user.profile_path}"
+                >
+                <td class="p-2 text-center"> ${counter}</td>
+                <td class="p-2 text-center"> ${user.title}</td>
+                <td class="p-2 text-center" style="text-decoration:ltr"> ${user.username}</td>
+                <td class="p-2 text-center"> <img class="userImage mx-2 mx-auto d-block" src='${user.profile_path}' /> </td>
+                <td class="p-2 text-center"> <input class="cursor-pointer" type="checkbox" name="honda" /> </td>
+                <td class="p-2 text-center"> <input class="cursor-pointer" type="checkbox" name="kia" /> </td>
+                <td class="p-2 text-center"> <input class="cursor-pointer" type="checkbox" name="chaines" /> </td>
+            </tr>`;
+            counter += 1;
+        }
+        contact.innerHTML = template;
+    }
+
     function openTab(tabId) {
         const tabs = document.querySelectorAll('.tab-content');
         tabs.forEach(tab => {
@@ -71,56 +131,6 @@ require_once('./app/Controllers/GoodController.php');
             }
         });
     }
-</script>
-<script>
-    const contact = document.getElementById('results_new');
-    contact.innerHTML = `
-            <tr>
-                <td colspan="7" class="py-5">
-                    <img class=' block w-10 mx-auto h-auto' src="./public/img/loading.png" />
-                </td>
-            </tr>
-            `;
-    axios.post("http://localhost/telegram/")
-        .then(function(response) {
-
-            displayData(response.data)
-        })
-        .catch(function(error) {
-
-        });
-
-
-    function displayData(data) {
-        let template = ``;
-        let counter = 1;
-        for (let user of data) {
-            template += `
-            <tr class="even:bg-indigo-100" data-user=" ${user.chat_id}">
-                <td class="p-2 text-center"> ${counter}</td>
-                <td class="p-2 text-center"> ${user.title}</td>
-                <td class="p-2 text-center" style="text-decoration:ltr"> ${user.username}</td>
-                <td class="p-2 text-center"> <img class="userImage mx-2 mx-auto d-block" src='${user.profile_path}' /> </td>
-                <td class="p-2 text-center"> <input type="checkbox" name="honda" /> </td>
-                <td class="p-2 text-center"> <input type="checkbox" name="kia" /> </td>
-                <td class="p-2 text-center"> <input type="checkbox" name="chaines" /> </td>
-            </tr>`;
-            counter += 1;
-        }
-        contact.innerHTML = template;
-    }
-
-    const tabs = document.querySelectorAll('[data-tab]');
-    tabs.forEach(tab => {
-        tab.addEventListener('click', () => {
-            tabs.forEach(t => t.classList.remove('active'));
-            tab.classList.add('active');
-            const target = document.querySelector(tab.dataset.target);
-            const panes = document.querySelectorAll('.tab-pane');
-            panes.forEach(pane => pane.classList.add('hidden'));
-            target.classList.remove('hidden');
-        });
-    });
 </script>
 <?php
 require_once('./views/Layouts/footer.php');
