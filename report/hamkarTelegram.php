6b9ea28d38e72737aff985c837e8a0a027c97242
MahdiSohaily <mahdi.sohaily4030@gmail.com>
2023-10-24 12:35:28 +0330

    Add script to always display the part number in uppercase text transform

--- report/hamkarTelegram.php
+++ report/hamkarTelegram.php
@@ -5,20 +5,29 @@ require_once('./views/Layouts/header.php');
 require_once('./app/Controllers/GoodController.php');
 ?>
 <div class="max-w-7xl my-5 mx-auto bg-white rounded-lg shadow-lg ">
-    <div class="flex rtl bg-gray-600  rounded-t-lg p-2">
-        <button class="px-4 py-2 rounded-lg bg-orange-100 text-orange-600 hover:bg-orange-300 ml-2 focus:outline-none" onclick="openTab('tab1')">لیست مخاطبین</button>
-        <button class="px-4 py-2 rounded-lg bg-orange-100 text-orange-600 hover:bg-orange-300 ml-2 focus:outline-none" onclick="openTab('tab2')">بروزرسانی لیست مخاطبین</button>
-        <button class="px-4 py-2 rounded-lg bg-orange-100 text-orange-600 hover:bg-orange-300 ml-2 focus:outline-none" onclick="openTab('tab3')">ارسال پیام</button>
+    <div class="flex rtl bg-violet-600  rounded-t-lg p-2">
+        <button class="px-4 py-2 rounded-lg bg-green-500 text-white hover:bg-green-300 ml-2 focus:outline-none" onclick="openTab('tab1')">لیست مخاطبین</button>
+        <button class="px-4 py-2 rounded-lg bg-green-500 text-white hover:bg-green-300 ml-2 focus:outline-none" onclick="openTab('tab2')">بروزرسانی لیست مخاطبین</button>
+        <button class="px-4 py-2 rounded-lg bg-green-500 text-white hover:bg-green-300 ml-2 focus:outline-none" onclick="openTab('tab3')">ارسال پیام</button>
     </div>
     <div class="p-4 rtl">
         <div id="tab1" class="tab-content">
-            <p>Content for Tab 1 goes here.</p>
+            <h1 class="text-xl py-2">لیست مخاطبین موجود در سیستم</h1>
+            <div class="bg-indigo-100">
+                content
+            </div>
         </div>
         <div id="tab2" class="tab-content hidden">
-            <p>Content for Tab 2 goes here.</p>
+            <h1 class="text-xl py-2">مخاطبین اخیر تلگرام</h1>
+            <div class="bg-indigo-100">
+                content
+            </div>
         </div>
         <div id="tab3" class="tab-content hidden">
-            <p>Content for Tab 2 goes here.</p>
+            <h1 class="text-xl py-2">ارسال پیام به گروه مخاطبین</h1>
+            <div class="bg-indigo-100">
+                content
+            </div>
         </div>
     </div>
 </div>
@@ -36,13 +45,13 @@ require_once('./app/Controllers/GoodController.php');
     }
 </script>
 <script>
-    // axios.post("http://localhost/telegram/")
-    //     .then(function(response) {
-    //         location.reload();
-    //     })
-    //     .catch(function(error) {
+    axios.post("http://localhost/telegram/")
+        .then(function(response) {
+            console.log(response.data);
+        })
+        .catch(function(error) {
 
-    //     });
+        });
 
     const tabs = document.querySelectorAll('[data-tab]');
     tabs.forEach(tab => {
