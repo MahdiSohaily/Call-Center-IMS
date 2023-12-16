f6cb20dc525b87636acdbefcddfe6ba19988dcd7
MahdiSohaily <mahdi.sohaily4030@gmail.com>
2023-12-16 14:56:44 +0330

    Add the save as PDF functionality

--- report/displayBill.php
+++ report/displayBill.php
@@ -3,6 +3,7 @@ require_once './config/config.php';
 require_once './database/connect.php';
 require_once('./views/Layouts/header.php');
 ?>
+<script src="./public/js/html2pdf.js"></script>
 <style>
     .bill {
         width: 800px;
@@ -81,6 +82,55 @@ require_once('./views/Layouts/header.php');
         padding: 3px 10px;
     }
 
+    .action_button {
+        width: 40px;
+        cursor: pointer;
+        height: 40px;
+        cursor: pointer
+    }
+
+    .action_menu {
+        position: fixed;
+        bottom: 20px;
+        right: 20px;
+        display: flex;
+        flex-direction: column;
+        gap: 10px;
+    }
+
+    .action_tooltip {
+        position: absolute;
+        width: 100px;
+        height: 36px;
+        border-bottom: 1px dotted black;
+        background-color: gray;
+        color: white;
+        padding: 5px 10px;
+        text-align: center;
+        right: 100%;
+        top: calc(50% - 18px);
+        font-size: 14px;
+        margin-right: 10px;
+        border-radius: 5px;
+        display: none;
+    }
+
+    .action_tooltip::after {
+        content: '';
+        position: absolute;
+        top: 50%;
+        right: -5px;
+        /* Adjust this value based on your design */
+        margin-top: -5px;
+        border-width: 5px 0 5px 5px;
+        border-style: solid;
+        border-color: transparent transparent transparent gray;
+    }
+
+    .action_button:hover+.action_tooltip {
+        display: block;
+    }
+
     @media print {
         * {
             font-size: 14px;
@@ -134,9 +184,12 @@ require_once('./views/Layouts/header.php');
             padding: 0 !important;
         }
 
+        .action_menu {
+            display: none;
+        }
     }
 </style>
-<div class="rtl bill">
+<div id="bill_body_pdf" class="rtl bill">
     <div class="bill_header">
         <div class="bill_info">
             <ul>
@@ -241,10 +294,26 @@ require_once('./views/Layouts/header.php');
         <p style="flex: 1;">امضاء فروشنده</p>
     </div>
 </div>
+<ul class="action_menu">
+    <li style="position: relative;">
+        <img class="action_button print" onclick="window.print();" src="./public/img/print.svg" alt="print icon">
+        <p class="action_tooltip">پرینت</p>
+    </li>
+    <li style="position: relative;">
+        <img class="action_button share" src="./public/img/share.svg" alt="print icon">
+        <p class="action_tooltip">اشتراک گذاری</p>
+    </li>
+    <li style="position: relative;">
+        <img class="action_button pdf" src="./public/img/pdf.svg" onclick="handleSaveAsPdfClick()" alt="print icon">
+        <p class="action_tooltip">پی دی اف</p>
+    </li>
+</ul>
 <script>
     const customerInfo = JSON.parse(localStorage.getItem('customer_info'));
     const BillInfo = JSON.parse(localStorage.getItem('bill_info'));
     const billItems = JSON.parse(localStorage.getItem('bill_items'));
+
+    console.log(billItems);
     displayBill();
     displayCustomer();
     displayBillDetails();
@@ -303,6 +372,41 @@ require_once('./views/Layouts/header.php');
         document.getElementById('discount').value = BillInfo.discount;
         document.getElementById('tax').value = BillInfo.tax;
         document.getElementById('total_in_word').innerHTML = BillInfo.totalInWords;
+    }
+
+    document.addEventListener('keydown', function(event) {
+        // Check if Ctrl (or Command on Mac) + P is pressed
+        if ((event.ctrlKey || event.metaKey) && (event.key === 'p' || event.keyCode === 80)) {
+            // Prevent the default action (in this case, printing)
+            event.preventDefault();
+            // Optionally, you can provide some feedback to the user
+            console.log('Printing is disabled.');
+        }
+    });
+
+    // Prevent context menu on right-click (optional)
+    document.addEventListener('contextmenu', function(event) {
+        event.preventDefault();
+    });
+
+    function handleSaveAsPdfClick() {
+        const content = document.getElementById('bill_body_pdf');
+        const opt = {
+            filename: 'myfile.pdf',
+            image: {
+                type: 'jpeg',
+                quality: 0.98
+            },
+            html2canvas: {
+                scale: 2
+            },
+            jsPDF: {
+                unit: 'in',
+                format: 'letter',
+                orientation: 'portrait'
+            }
+        };
+        html2pdf().set(opt).from(content).save();
     }
 </script>
 <?php
