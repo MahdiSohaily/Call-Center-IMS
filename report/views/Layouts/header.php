f7b1f0969e0b072af252d5abfb191eb5210ec69d
MahdiSohaily <mahdi.sohaily4030@gmail.com>
2023-12-29 14:47:17 +0330

    Fixed the items count bug in invoice

--- report/views/Layouts/header.php
+++ report/views/Layouts/header.php
@@ -112,7 +112,7 @@ $_SESSION["user_id"] = $_SESSION["id"];
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link href="./public/css/material_icons.css" rel="stylesheet">
     <script src="./public/js/index.js"></script>
     <link rel="stylesheet" href="./public/css/styles.css?v=<?= rand() ?>">
     <script src="./public/js/axios.js"></script>
@@ -443,4 +443,3 @@ $_SESSION["user_id"] = $_SESSION["id"];
                 </a>
             </div>
         </div>
-       
