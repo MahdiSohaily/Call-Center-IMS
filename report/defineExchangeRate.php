861d6321676f45dd1625790e33a25d3b99d408fa
MahdiSohaily <mahdi.sohaily4030@gmail.com>
2024-03-04 11:21:58 +0330

    Update input field IDs in defineExchangeRate.php

--- report/defineExchangeRate.php
+++ report/defineExchangeRate.php
@@ -104,16 +104,16 @@ require_once './app/Controllers/DollarRateController.php';
         </div>
         <div class="flex flex-wrap mb-6">
             <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
-                <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="rate">
+                <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="edit_rate">
                     درصد دلار
                 </label>
-                <input name="rate" value="" class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" type="text" placeholder="درصد دلار" required>
+                <input id="edit_rate" name="rate" value="" class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" type="text" placeholder="درصد دلار" required>
             </div>
             <div class="w-full md:w-1/2 px-3">
-                <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-last-name">
+                <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="edit_date">
                     بازه اول
                 </label>
-                <input name="date" value="" class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-500 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" type="date" placeholder="اعمال تا تاریخ" required>
+                <input id="edit_date" name="date" value="" class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-500 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" type="date" placeholder="اعمال تا تاریخ" required>
             </div>
             <div class="w-full md:w-1/2 px-3">
                 <button class=" shadow bg-blue-500 hover:bg-blue-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded" type="submit">
