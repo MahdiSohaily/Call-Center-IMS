7155a82b9d6a0a8cc6b5056de7aa779d23f95a60
MahdiSohaily <mahdi.sohaily4030@gmail.com>
2024-03-04 11:20:47 +0330

    Update button class names in defineExchangeRate.php

--- report/defineExchangeRate.php
+++ report/defineExchangeRate.php
@@ -50,7 +50,7 @@ require_once './app/Controllers/DollarRateController.php';
                                     فعال سازی
                                 </button>
                             <?php endif; ?>
-                            <button onclick="toggleActivation(<?= $id ?>, 0)" class="shadow bg-blue-500 hover:bg-blue-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded">
+                            <button onclick="editItem(<?= $id ?>)" class="shadow bg-blue-500 hover:bg-blue-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded">
                                 ویرایش
                             </button>
                         </td>
@@ -60,7 +60,7 @@ require_once './app/Controllers/DollarRateController.php';
         </table>
     </div>
 </div>
-<div id="insert_new" class="my-5 bg-white rounded-lg shadow-md w-1/2 mx-auto p-5">
+<div id="insert_block" class="my-5 bg-white rounded-lg shadow-md w-1/2 mx-auto p-5">
     <div class="rtl">
         <div class="rtl flex items-center justify-between mb-4">
             <h2 class="text-xl font-semibold text-gray-800 flex items-center gap-2">
@@ -94,41 +94,40 @@ require_once './app/Controllers/DollarRateController.php';
         </form>
     </div>
 </div>
-<div id="edit_existing" class="hidden my-5 bg-white rounded-lg shadow-md w-1/2 mx-auto p-5">
+<div id="edit_block" class="hidden my-5 bg-white rounded-lg shadow-md w-1/2 mx-auto p-5">
     <div class="rtl">
         <div class="rtl flex items-center justify-between mb-4">
             <h2 class="text-xl font-semibold text-gray-800 flex items-center gap-2">
-                تعریف درصد تغییر قیمت دلار
+                ویرایش درصد تغییر قیمت دلار
                 <i class="material-icons font-semibold text-indigo-400">create</i>
             </h2>
         </div>
-        <form class="w-full" action=<?= htmlspecialchars($_SERVER['PHP_SELF']) ?> method="post">
-            <div class="flex flex-wrap mb-6">
-                <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
-                    <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="rate">
-                        درصد دلار
-                    </label>
-                    <input name="rate" value="" class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" id="rate" type="text" placeholder="درصد دلار" required>
-                </div>
-                <div class="w-full md:w-1/2 px-3">
-                    <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-last-name">
-                        بازه اول
-                    </label>
-                    <input name="date" value="" class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-500 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-last-name" type="date" placeholder="اعمال تا تاریخ" required>
-                </div>
-                <div class="w-full md:w-1/2 px-3">
-                    <button class=" shadow bg-green-500 hover:bg-green-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded" type="submit">
-                        ثبت سیستم
-                    </button>
-                    <?php if ($status) : ?>
-                        <p class="text-green-600 ">ویرایش موفقانه صورت گرفت.</p>
-                    <?php endif; ?>
-                </div>
+        <div class="flex flex-wrap mb-6">
+            <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+                <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="rate">
+                    درصد دلار
+                </label>
+                <input name="rate" value="" class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" type="text" placeholder="درصد دلار" required>
             </div>
+            <div class="w-full md:w-1/2 px-3">
+                <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-last-name">
+                    بازه اول
+                </label>
+                <input name="date" value="" class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-500 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" type="date" placeholder="اعمال تا تاریخ" required>
+            </div>
+            <div class="w-full md:w-1/2 px-3">
+                <button class=" shadow bg-blue-500 hover:bg-blue-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded" type="submit">
+                    ویرایش
+                </button>
+            </div>
+        </div>
         </form>
     </div>
 </div>
 <script>
+    const insertBlock = document.getElementById('insert_block');
+    const editBlock = document.getElementById('edit_block');
+
     function toggleActivation(id, type) {
         const params = new URLSearchParams();
         params.append("toggleActivation", "toggleActivation");
@@ -143,6 +142,12 @@ require_once './app/Controllers/DollarRateController.php';
                 }
             })
             .catch(function(error) {});
+    }
+
+
+    function editItem(id) {
+        editBlock.classList.remove('hidden');
+        insertBlock.classList.add('hidden');
     }
 </script>
 <?php
