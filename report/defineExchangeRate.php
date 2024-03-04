73ec80da91502629cb017f8fc7303388f9020462
MahdiSohaily <mahdi.sohaily4030@gmail.com>
2024-03-04 11:01:15 +0330

    Fix button labels and add new button for editing exchange rate

--- report/defineExchangeRate.php
+++ report/defineExchangeRate.php
@@ -42,14 +42,17 @@ require_once './app/Controllers/DollarRateController.php';
                         <td class='p-2 rtl text-center'>
                             <?php if ($rate['status']) :
                                 $id = $rate['id'] ?>
-                                <button onclick="toggleActivation('<?= $id ?>', 0)" class=" shadow bg-red-500 hover:bg-red-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded" type="submit">
+                                <button onclick="toggleActivation(<?= $id ?>, 0)" class="shadow bg-red-500 hover:bg-red-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded">
                                     غیر فعال سازی
                                 </button>
                             <?php else : ?>
-                                <button onclick="toggleActivation('<?= $id ?>', 1)" class=" shadow bg-green-500 hover:bg-green-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded" type="submit">
+                                <button onclick="toggleActivation(<?= $id ?>, 1)" class="shadow bg-green-500 hover:bg-green-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded">
                                     فعال سازی
                                 </button>
                             <?php endif; ?>
+                            <button onclick="toggleActivation(<?= $id ?>, 0)" class="shadow bg-blue-500 hover:bg-blue-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded">
+                                ویرایش
+                            </button>
                         </td>
                     </tr>
                 <?php endforeach; ?>
@@ -61,27 +64,27 @@ require_once './app/Controllers/DollarRateController.php';
     <div class="rtl">
         <div class="rtl flex items-center justify-between mb-4">
             <h2 class="text-xl font-semibold text-gray-800 flex items-center gap-2">
-                ویرایش درصد تغییر قیمت دلار
+                تعریف درصد تغییر قیمت دلار
                 <i class="material-icons font-semibold text-indigo-400">create</i>
             </h2>
         </div>
         <form class="w-full" action=<?= htmlspecialchars($_SERVER['PHP_SELF']) ?> method="post">
             <div class="flex flex-wrap mb-6">
                 <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
-                    <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-first-name">
+                    <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="rate">
                         درصد دلار
                     </label>
-                    <input name="rate" value="<?= $dollarRate['rate'] ?>" class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" id="grid-first-name" type="text" placeholder="درصد دلار" required>
+                    <input name="rate" value="" class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" id="rate" type="text" placeholder="درصد دلار" required>
                 </div>
                 <div class="w-full md:w-1/2 px-3">
                     <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-last-name">
                         بازه اول
                     </label>
-                    <input name="date" value="<?= $dollarRate['created_at'] ?>" class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-500 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-last-name" type="date" placeholder="اعمال تا تاریخ" required>
+                    <input name="date" value="" class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-500 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-last-name" type="date" placeholder="اعمال تا تاریخ" required>
                 </div>
                 <div class="w-full md:w-1/2 px-3">
-                    <button class=" shadow bg-purple-500 hover:bg-purple-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded" type="submit">
-                        ویرایش
+                    <button class=" shadow bg-green-500 hover:bg-green-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded" type="submit">
+                        ثبت سیستم
                     </button>
                     <?php if ($status) : ?>
                         <p class="text-green-600 ">ویرایش موفقانه صورت گرفت.</p>
